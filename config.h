2cefdb4b6e4b76fab259e0e69216bd59f10e3ecf
Francis <francisklinck@gmail.com>
2020-05-22 18:58:38 +0000

    Fix rendering of some java programs

--- config.h
+++ config.h
@@ -28,8 +28,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",     	    NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  	    NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "idea",	    NULL,       "LG3D",     0,            0,           -1 },
+    	{ "clion",          NULL,       "LG3D",     0,            0,           -1 },
+    	{ "idea-ultimate",  NULL,       "LG3D",     0,            0,           -1 },
+    	{ "ghidra",         NULL,       "LG3D",     0,            0,           -1 },
 };
 
 /* layout(s) */
