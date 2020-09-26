f296aabec896cd40971b43bfb959b02564ab4d90
Francis <francisklinck@gmail.com>
2020-09-26 14:53:13 +0200

    change some config values

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx	    = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -12,7 +12,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#06ba00";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -29,7 +29,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     	    NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  	    NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  	    NULL,       NULL,       0,            0,           -1 },
 	{ "idea",	    NULL,       "LG3D",     0,            0,           -1 },
     	{ "clion",          NULL,       "LG3D",     0,            0,           -1 },
     	{ "idea-ultimate",  NULL,       "LG3D",     0,            0,           -1 },
